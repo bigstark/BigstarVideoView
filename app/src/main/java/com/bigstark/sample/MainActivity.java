1c43e3f249e22d1569e979f88d5e096dc9d9d5c7
BigStarK <bigstark@makeus.com>
2016-01-07 21:30:29 +0900

    add sample code

--- app/src/main/java/com/bigstark/sample/MainActivity.java
+++ app/src/main/java/com/bigstark/sample/MainActivity.java
@@ -1,6 +1,22 @@
 package com.bigstark.sample;
 
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.bigstark.video.BigstarVideoView;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final String SAMPLE_URL = "SAMPLE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BigstarVideoView videoView = (BigstarVideoView) findViewById(R.id.videoview);
+        videoView.setRetainPlayerInstance(true);
+        videoView.setVideoURI(Uri.parse(SAMPLE_URL), true);
+    }
 }
